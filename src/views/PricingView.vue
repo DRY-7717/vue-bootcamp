5ad0c42bd7663ae8b6b4df4bb74353baff4540a7
DRY-7717 <wicaksanabimaarya01@gmail.com>
2024-03-17 03:57:52 +0700

    menambahkan file redirects

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -111,10 +111,7 @@
                                     class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
                                     Checkout Now
                                 </button>
-                                <button @click="test()"
-                                    class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
-                                    test
-                                </button>
+                               
                             </div>
                         </div>
                     </div>
@@ -215,9 +212,5 @@ async function checkout(price) {
 }
 
 
-function test() {
-    const url = 'https://buildwithangga.com/'
-    window.location.href = url
-}
 
 </script>
